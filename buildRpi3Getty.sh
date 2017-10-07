203a5a5f75958d9893470e1b0c7583b9eb7dcc1f
Isaac Rodman <isaac@eyz.us>
2017-10-07 21:56:15 +0000

    1) added buildRpi3Getty.sh script (to be run from project root) 2) created new examples/gettyInInitWithFirmwareModulesAndDhcpcd.yml (as it describes) 3) built examples/gettyInInitWithFirmwareModulesAndDhcpcd-initrd.img

---
+++ buildRpi3Getty.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+### build and tag linuxkit-rpi3/getty:0.1
+
+cd pkg/getty
+docker build --pull -t linuxkit-rpi3/getty:0.1 .
+
+### build gettyInInitWithFirmwareModulesAndDhcpcd initramfs (initrd)
+
+cd ../../examples
+
+for IMAGE in $(grep -Eo 'linuxkit/.+$' gettyInInitWithFirmwareModulesAndDhcpcd.yml); do
+  docker pull "${IMAGE}"
+done
+
+moby build -disable-content-trust -format kernel+initrd gettyInInitWithFirmwareModulesAndDhcpcd.yml
+
